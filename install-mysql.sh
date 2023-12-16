9cb745df9201d802643f92486c5b8e54a37b3c5c
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2023-12-16 19:29:33 +0530

    added conditions

---
+++ install-mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: installing mysql is failed"
+    exit 1
+else
+    echo "installing mysql is success"
+fi        
