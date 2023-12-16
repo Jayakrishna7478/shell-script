96ef9fc58026a58c3c2615746ea2a60c53d88f32
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2023-12-16 21:18:33 +0530

    added conditions

---
+++ logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(data +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 ... FAILED"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "installing MYSQL"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "installing GIT"
