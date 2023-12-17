1663a231245b730ec4c8e05b7dac323f66ac1b9f
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2023-12-17 18:39:55 +0530

    added roboshop

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1-ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi        
+}
 
 if [ $ID -ne 0 ]
 then
@@ -10,4 +28,16 @@ else
     echo "you are root user"
 fi
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE #check install or not
+    if [ $? -ne 0 ] # if not installed
+    then
+        yum install $package -y&>> $LOGFILE # install the packge
+        VALIDATE $? "installation of $packge" # validate
+    else
+        echo -e "$packge  is already installed ... $Y SKIPPING $N"
+    fi
+done
